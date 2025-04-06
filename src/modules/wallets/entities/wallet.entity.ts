c176fb207b186afb6e8eb0beb818bf8b3e3764df
flmpl <flmp.leonardo@gmail.com>
2025-04-06 19:33:40 -0300

    [ADD: Wallets

---
+++ src/modules/wallets/entities/wallet.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Wallet {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  balance: number;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
+}
